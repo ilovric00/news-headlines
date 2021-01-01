0ebd95734264680fc0fe16a7260b9f89bed06e19
ilovric00 <ilovric@profico.hr>
2021-01-01 20:52:59 +0100

    ESLint, Stylelint and Prettier configuration

--- components/HeroPost/HeroPost.tsx
+++ components/HeroPost/HeroPost.tsx
@@ -1,49 +1,45 @@
-import Avatar from '../Avatar'
-import DateFormatter from '../DateFormatter'
-import CoverImage from '../CoverImage'
-import Link from 'next/link'
-import Author from '../../types/author'
+import Avatar from '../Avatar';
+import DateFormatter from '../DateFormatter';
+import CoverImage from '../CoverImage';
+import Hyperlink from '../Hyperlink';
+import Author from '../../types/author';
 
 type Props = {
-  title: string
-  coverImage: string
-  date: string
-  excerpt: string
-  author: Author
-  slug: string
-}
+  title: string;
+  coverImage: string;
+  date: string;
+  excerpt: string;
+  author: Author;
+  slug: string;
+};
 
-const HeroPost = ({
-  title,
-  coverImage,
-  date,
-  excerpt,
-  author,
-  slug,
-}: Props) => {
-  return (
-    <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage title={title} src={coverImage} slug={slug} />
-      </div>
-      <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
-        <div>
-          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link as={`/posts/${slug}`} href="/posts/[slug]">
-              <a className="hover:underline">{title}</a>
-            </Link>
-          </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <DateFormatter dateString={date} />
-          </div>
-        </div>
-        <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          <Avatar name={author.name} picture={author.picture} />
+const HeroPost = ({ title, coverImage, date, excerpt, author, slug }: Props) => (
+  <section>
+    <div className="mb-8 md:mb-16">
+      <CoverImage title={title} src={coverImage} slug={slug} />
+    </div>
+    <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
+      <div>
+        <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
+          <Hyperlink
+            as={`/posts/${slug}`}
+            href="/posts/[slug]"
+            AnchorProps={{
+              className: 'hover:underline',
+              dangerouslySetInnerHTML: { __html: title },
+            }}
+          />
+        </h3>
+        <div className="mb-4 md:mb-0 text-lg">
+          <DateFormatter dateString={date} />
         </div>
       </div>
-    </section>
-  )
-}
+      <div>
+        <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+        <Avatar name={author.name} picture={author.picture} />
+      </div>
+    </div>
+  </section>
+);
 
-export default HeroPost
+export default HeroPost;
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,27 +1,27 @@
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
-import Container from '../../components/Container'
-import PostBody from '../../components/PostBody'
-import Header from '../../components/Header'
-import PostHeader from '../../components/PostHeader'
-import Layout from '../../components/Layout'
-import { getPostBySlug, getAllPosts } from '../../lib/api'
-import PostTitle from '../../components/PostTitle'
-import Head from 'next/head'
-import { CMS_NAME } from '../../lib/constants'
-import markdownToHtml from '../../lib/markdownToHtml'
-import PostType from '../../types/post'
+import { useRouter } from 'next/router';
+import ErrorPage from 'next/error';
+import Head from 'next/head';
+import Container from '../../components/Container';
+import PostBody from '../../components/PostBody';
+import Header from '../../components/Header';
+import PostHeader from '../../components/PostHeader';
+import Layout from '../../components/Layout';
+import { getPostBySlug, getAllPosts } from '../../lib/api';
+import PostTitle from '../../components/PostTitle';
+import { CMS_NAME } from '../../lib/constants';
+import markdownToHtml from '../../lib/markdownToHtml';
+import PostType from '../../types/post';
 
 type Props = {
-  post: PostType
-  morePosts: PostType[]
-  preview?: boolean
-}
+  post: PostType;
+  // morePosts: PostType[];
+  preview?: boolean;
+};
 
-const Post = ({ post, morePosts, preview }: Props) => {
-  const router = useRouter()
+const Post = ({ post, preview }: Props) => {
+  const router = useRouter();
   if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
+    return <ErrorPage statusCode={404} />;
   }
   return (
     <Layout preview={preview}>
@@ -34,44 +34,32 @@ const Post = ({ post, morePosts, preview }: Props) => {
             <article className="mb-32">
               <Head>
                 <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
+                  {post.title} | Next.js Blog Example with
+                  {CMS_NAME}
                 </title>
                 <meta property="og:image" content={post.ogImage.url} />
               </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
+              <PostHeader title={post.title} coverImage={post.coverImage} date={post.date} author={post.author} />
               <PostBody content={post.content} />
             </article>
           </>
         )}
       </Container>
     </Layout>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
 
 type Params = {
   params: {
-    slug: string
-  }
-}
+    slug: string;
+  };
+};
 
 export async function getStaticProps({ params }: Params) {
-  const post = getPostBySlug(params.slug, [
-    'title',
-    'date',
-    'slug',
-    'author',
-    'content',
-    'ogImage',
-    'coverImage',
-  ])
-  const content = await markdownToHtml(post.content || '')
+  const post = getPostBySlug(params.slug, ['title', 'date', 'slug', 'author', 'content', 'ogImage', 'coverImage']);
+  const content = await markdownToHtml(post.content || '');
 
   return {
     props: {
@@ -80,20 +68,18 @@ export async function getStaticProps({ params }: Params) {
         content,
       },
     },
-  }
+  };
 }
 
 export async function getStaticPaths() {
-  const posts = getAllPosts(['slug'])
+  const allPosts = getAllPosts(['slug']);
 
   return {
-    paths: posts.map((posts) => {
-      return {
-        params: {
-          slug: posts.slug,
-        },
-      }
-    }),
+    paths: allPosts.map(posts => ({
+      params: {
+        slug: posts.slug,
+      },
+    })),
     fallback: false,
-  }
+  };
 }
