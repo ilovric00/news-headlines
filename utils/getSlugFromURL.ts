c175b78f863f9364a63891b9103d1c43c0af70b1
ilovric00 <ilovric@profico.hr>
2021-01-21 19:55:19 +0100

    Bugfix: regex to remove file extension from JS string

--- utils/getSlugFromURL.ts
+++ utils/getSlugFromURL.ts
@@ -1,7 +1,10 @@
 const getSlugFromURL = (url: string): string => {
   const urlSubstrings = url.split('/');
   const { length } = urlSubstrings;
-  return urlSubstrings[length - 1] !== '' ? urlSubstrings[length - 1] : urlSubstrings[length - 2];
+  return (urlSubstrings[length - 1] !== '' ? urlSubstrings[length - 1] : urlSubstrings[length - 2]).replace(
+    /\.[^/.]+$/,
+    ''
+  );
 };
 
 export default getSlugFromURL;
